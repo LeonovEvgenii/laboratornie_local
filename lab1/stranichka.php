0f3beba6a17584b4d5f3ff396a12d70a1c4d4c7a
LeonovEvgenii <leonovevgenii2010@mail.ru>
2015-04-24 11:57:30 +0600

    Лабораторная №1
    Леонов Евгений Русланович
    АСУ - 12
    Сделал 4 прямоугольника: название сайта, навигация, место для
    содержания, информация о сайте внизу.
    В навигации ссылка на оглавление, картинки(страница содержит две
    картинки и подиси), ссылки (содержит название картинок).

---
+++ lab1/stranichka.php
@@ -0,0 +1,66 @@
+﻿<!DOCTYPE HTML>
+<html>
+
+<head>
+
+<meta charset="utf-8">
+<title>web - страничка</title>
+<link rel="stylesheet" href="style_stranichka.css">
+
+</head>
+<body>
+
+	<header>
+
+		<div class="text" >Чертежи</div>	
+
+	</header>
+
+	<nav>
+	
+		<div class="text" >Навигация</div>
+		<li>
+			<a href="http://localhost/stranichka.php">К оглавлению</a>
+		</li>
+		<li>
+			<a href="http://localhost/stranichka_kartinki.php">Картинки</a>
+		</li>
+		<li>
+			<a href="http://localhost/stranichka_ssilki.php">Ссылки</a>
+		</li>
+		<div class="text" >Регистрация</div>
+		Email
+		<br>
+		<input type="text" size="24">
+		<br>
+		Логин
+		<br>
+		<input type="text" size="24">
+		<br>
+		Телефон
+		<br>
+		<input type="text" size="24">
+		<br>
+		Пароль
+		<br>
+		<input type="text" size="24">
+		<br>	
+		<button>Регистрация</button>
+		
+	</nav>
+
+	<article>
+		
+		<div class="text" >Содержание</div>
+		
+	</article>
+
+	<footer>
+ 
+		<div class="text" >Информация</div>
+ 
+	</footer>
+
+</body>
+
+</html>
---
+++ lab1/stranichka_ssilki.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE HTML>
+<html>
+
+<head>
+
+<meta charset="utf-8">
+<title>web - страничка</title>
+<link rel="stylesheet" href="style_stranichka.css">
+
+</head>
+<body>
+
+	<header>
+
+		<div class="text" >Чертежи</div>	
+
+	</header>
+
+	<nav>
+	
+		<div class="text" >Навигация</div>
+		<li>
+			<a href="http://localhost/stranichka.php">К оглавлению</a>
+		</li>
+		<li>
+			<a href="http://localhost/stranichka_kartinki.php">Картинки</a>
+		</li>
+		<li>
+			Ссылки
+		</li>
+	
+	</nav>
+
+	<article>
+		
+		<div class="text" >Содержание</div>
+		<li>
+		Пулемет
+		</li>
+		<li>
+		Багги
+		</li>
+		
+	</article>
+
+	<footer>
+ 
+		<div class="text" >Информация</div>
+ 
+	</footer>
+
+</body>
+
+</html>
